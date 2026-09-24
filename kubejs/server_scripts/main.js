2910abb3ab5452d21e247e8d83467c707671184c
Pingus1000 <p.riedel.03771@gmail.com>
2026-09-24 22:36:47 +0200

    KubeJS: GIG

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -2,10 +2,17 @@
 console.info('Hello, World! (Loaded server example script)')
 
 
+const GIG_REPLIES = [
+  'Gax ist Grün',
+  'Gemoded ist geil',
+  'Gax ist Geldgeil'
+]
+
 PlayerEvents.chat(event => {
   if (event.message == 'GIG') {
+    let reply = GIG_REPLIES[Math.floor(Math.random() * GIG_REPLIES.length)]
     event.server.scheduleInTicks(5, () => {
-      event.server.tell(Text.green('Gax ist Grün'))
+      event.server.tell(Text.green(reply))
     })
   }
 })
